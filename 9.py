6701e5bd990df1c256c5c77cbd2024f4c6f50bb3
unknown <dmitry.makovetski@gmail.com>
2023-01-01 21:50:30 +0200

    second commit

--- 9.py
+++ 9.py
@@ -1,24 +1,20 @@
 class Auto():
-
     def __init__(self, brand, сolour, engine_volume):
         self.brand = brand
         self.сolour = сolour
         self.engine_volume = engine_volume
         self.position_transmission = None
         self.brake_position = None
-
     def go_forward(self, position_transmission, brake_position):
         if self.position_transmission > 0 and self.brake_position==0:
             print("You are moving forward.")
         else:
             print("Take your foot off the brake pedal and shift into gear.")
-
     def go_back(self, position, brake_position):
         if self.position_transmission < 0 and self.brake_position == 0:
             print("You are moving back.")
         else:
             print("Take your foot off the brake pedal and engage reverse gear.")
-
 
 class Auto_NF(Auto):
 
@@ -33,3 +29,49 @@ class Auto_NF(Auto):
             print("You are moving straight.")
         else:
             print("You turn left.")
+
+
+class TextProcessor():
+    def __init__(self, in_string):
+        self.in_string=in_string
+    @property
+    def is_punktiantian(self, i_in_string):
+        i=i_in_string
+        if i== "." or i== "," or i==":" or i==";" or i=="?" or i=="!" or i=="-" or i=="(" or i==")" or i==" " "
+            self._a=True
+        else:
+            self._a=False
+        return self._a
+    def get_clean_string(self, in_string):
+        for i in self.in_string:
+            if is_punktiantian(i)==True
+                i=" "
+            str_new+=i
+        return str_new
+
+
+class TextLoader():
+    def __init__(self, __text_processor, __clean_string):
+        self.__text_processor=TextProcessor()
+        self.__clean_string=__clean_string
+    @property
+    def set_clean_text(self):
+        self.__clean_string=self.__text_processor
+        return self.__clean_string
+    @property
+    def clean_string(self):
+        print("Text already cleared.")
+
+
+
+class Parallelogram():
+    def __init__(self, width, length):
+        self.width=width
+        self.length=length
+    def get_area(self):
+        return self.width*self.length
+class Square(Parallelogram):
+    def __init__(self):
+        Parallelogram.__init__(self)
+    def get_area(self):
+        return self.length**2
